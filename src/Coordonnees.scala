689c9283787d64740aa8984cb1c1e61e678c186b
Hass92 <46720199+Hass92@users.noreply.github.com>
2019-12-08 00:02:59 +0100

    Add files via upload

---
+++ src/Coordonnees.scala
@@ -0,0 +1,8 @@
+/**
+ * Projet Scala Master 2 IMSD - 2019/2020
+ * Enseignant : Mourad Karoui
+ * @authors Abdou Lahat DIOP, Hassnaini Abdourahamane et Amadi DIOP
+ */
+class Coordonnees(var coorX: Int, var coorY: Int) {
+
+}
